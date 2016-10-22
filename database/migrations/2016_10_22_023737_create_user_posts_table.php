0dd873af594caf50ea163af458c1b3c8cb974c6c
Juan Camilo Mora Vélez <jcmlmorav@gmail.com>
2016-10-21 21:49:20 -0500

    Migrations created

---
+++ database/migrations/2016_10_22_023737_create_user_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->string('thumbnail');
+            $table->boolean('active')->default(true);
+            $table->integer('user')->unsigned();
+            $table->integer('post')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_posts');
+    }
+}
